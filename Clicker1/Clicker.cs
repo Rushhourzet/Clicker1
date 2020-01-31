af55275db42bd51368bc15d10ace7962a7ca7e13
Rushhourz <sascha.hampl@outlook.com>
2020-01-31 12:14:59 +0100

    created Background, Button

--- Clicker1/Clicker.cs
+++ Clicker1/Clicker.cs
@@ -5,13 +5,19 @@ public class Clicker : Control
 {
 	private double value = 0.0;
 	private double power = 1.0;
+	private Label valueLabel;
 	public override void _Ready()
 	{
-		
+		valueLabel = (Label)GetNode("ValueDisplay");
 	}
 
 	private void _on_ClickButton_pressed()
 	{
 		value += power;
+		RefreshLabel();
+	}
+	
+	private void RefreshLabel(){
+		valueLabel.Text = value + " Mana";
 	}
 }
